459eeae2896b1ce95f7f478fa414de262c0a3634
Maxim Milovanov <mma29121983@gmail.com>
2019-03-06 22:21:55 -0500

    adds trip-details card

---
+++ src/components/trip-details-card.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="horizontal-container">
+
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue, Emit} from 'vue-property-decorator';
+import { IDetailsSection } from '..';
+
+let uniqueId = 0;
+
+@Component({
+  components: {
+
+  },
+})
+export default class TripOptionDetailsCardComponent extends Vue {
+  public $refs!: {
+    container: HTMLDivElement;
+  };
+
+  @Prop({default: []})
+  public sections!: IDetailsSection[];
+
+  @Prop({default: 10})
+  public gap!: number;
+
+  @Prop({default: 'gray'})
+  public gapColor!: string;
+
+  @Prop({default: 0})
+  public maxWidth!: number;
+
+  @Prop({default() {
+    return Math.random();
+  }})
+  public updateTrigger!: number;
+
+  @Prop({
+    default() {
+      return `trip-option-detils-card-${uniqueId++}`;
+    },
+  })
+  public id!: string;
+  private isReady: boolean = false;
+
+
+
+  public mounted() {
+    this.isReady = true;
+  }
+
+  private get isValid(): boolean {
+    return this.sections.length > 0;
+  }
+}
+</script>
+
+
+<style lang="postcss" scoped>
+.horizontal-container {
+  width: 100%;
+  overflow: hidden;
+  padding: 1em;
+}
+</style>
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import TripOptionDetailsComponent from './components/trip-details.vue';
+import TripOptionDetailsCardComponent from './components/trip-details-card.vue';
 export {IDetailsSection} from './types/details-section.interface';
-export {TripOptionDetailsComponent};
+export {TripOptionDetailsComponent, TripOptionDetailsCardComponent};
