f56f8344548c38f7ec07abccbf0cc6026e9842f2
Maxim Milovanov <mma29121983@gmail.com>
2019-03-08 22:32:27 -0500

    exports additional type

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import TripOptionDetailsComponent from './components/trip-details.vue';
 import TripOptionDetailsCardComponent from './components/trip-details-card.vue';
 import TripOptionDetailsListComponent from './components/trip-details-list.vue';
 export {IDetailsSection} from './types/details-section.interface';
+export {ITripOption} from './types/trip-option.interface';
 export {TripOptionDetailsComponent, TripOptionDetailsCardComponent, TripOptionDetailsListComponent};
